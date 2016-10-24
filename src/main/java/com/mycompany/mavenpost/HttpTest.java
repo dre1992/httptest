ae26e5f46c0f3f50fcdf66ff3e5af15aad1e0bfc
Andreas Seficha <andreas.seficha@marketplace-analytics.de>
2016-10-24 17:20:18 +0200

    +comments for proper network handling

--- src/main/java/com/mycompany/mavenpost/HttpTest.java
+++ src/main/java/com/mycompany/mavenpost/HttpTest.java
@@ -82,7 +82,7 @@ for (Header header : headers) {
 	      + " ,Value : " + header.getValue());
 }
  if (entity != null) {
-           String retSrc = EntityUtils.toString(entity); 
+           String retSrc = EntityUtils.toString(entity); //Discouraged better open a stream and read the data as per Apache manual                     
            // parsing JSON
            //JSONObject result = new JSONObject(retSrc);
            Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -109,4 +109,30 @@ System.out.println(prettyJsonString);
 }
 
 
-
+//prefered way to deal with responses
+//CloseableHttpClient httpclient = HttpClients.createDefault();
+//HttpGet httpget = new HttpGet("http://localhost/json");
+//
+//ResponseHandler<MyJsonObject> rh = new ResponseHandler<MyJsonObject>() {
+//
+//    @Override
+//    public JsonObject handleResponse(
+//            final HttpResponse response) throws IOException {
+//        StatusLine statusLine = response.getStatusLine();
+//        HttpEntity entity = response.getEntity();
+//        if (statusLine.getStatusCode() >= 300) {
+//            throw new HttpResponseException(
+//                    statusLine.getStatusCode(),
+//                    statusLine.getReasonPhrase());
+//        }
+//        if (entity == null) {
+//            throw new ClientProtocolException("Response contains no content");
+//        }
+//        Gson gson = new GsonBuilder().create();
+//        ContentType contentType = ContentType.getOrDefault(entity);
+//        Charset charset = contentType.getCharset();
+//        Reader reader = new InputStreamReader(entity.getContent(), charset);
+//        return gson.fromJson(reader, MyJsonObject.class);
+//    }
+//};
+//MyJsonObject myjson = client.execute(httpget, rh);
